b3f0089359d1219584fb6957eabb3c394175a66a
Daniil Makovetskii <145105152+DaniilMako@users.noreply.github.com>
2024-03-03 01:22:11 +0700

    Delete build directory

--- build/CMakeFiles/bitArray.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for bitArray.
-# This may be replaced when dependencies are built.
--- build/tests/tests[1]_include.cmake
+++
@@ -1,5 +0,0 @@
-if(EXISTS "/mnt/d/Загрузки/OOP-main/BitArray/build/tests/tests[1]_tests.cmake")
-  include("/mnt/d/Загрузки/OOP-main/BitArray/build/tests/tests[1]_tests.cmake")
-else()
-  add_test(tests_NOT_BUILT tests_NOT_BUILT)
-endif()
